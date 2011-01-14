d591e0f316b29c7b8e1fc2fb83c388674c86526d
Hyungsuk Hong <aanoaa@gmail.com>
2011-01-14 10:27:29 +0900

    added custom toast ui

--- apps/android/src/kr/perl/android/logviewer/util/ContextUtil.java
+++ apps/android/src/kr/perl/android/logviewer/util/ContextUtil.java
@@ -1,9 +1,15 @@
 package kr.perl.android.logviewer.util;
 
+import kr.perl.android.logviewer.R;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public final class ContextUtil {
@@ -18,7 +24,23 @@ public final class ContextUtil {
 	}
 	
 	public static void toast(Activity activity, String message) {
-		Toast.makeText(activity.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+		final View view = LayoutInflater.from(activity.getApplicationContext()).inflate(R.layout.custom_toast, null);
+		((TextView) view.findViewById(R.id.text1)).setText(message);
+		
+		Toast toast = new Toast(activity.getApplicationContext());
+		toast.setDuration(Toast.LENGTH_SHORT);
+		toast.setView(view);
+		toast.show();
+	}
+	
+	public static void showToast(Context context, int resourceId, int id, Drawable drawable, Object... args) {
+		final View view = LayoutInflater.from(context).inflate(resourceId, null);
+		((ImageView) view.findViewById(id)).setImageDrawable(drawable);
+		
+		Toast toast = new Toast(context);
+		toast.setDuration(Toast.LENGTH_LONG);
+		toast.setView(view);
+		toast.show();
 	}
 	
 	public static void toastOnUiTread(Activity activity, String message) {
