0181d50012e26ac5bd0f9567570671f3b2c8c6b4
Hyungsuk Hong <aanoaa@gmail.com>
2012-02-28 15:13:31 +0900

    remove needless sync option

--- apps/web/hubot-viewer/scripts/collections/logs.coffee
+++ apps/web/hubot-viewer/scripts/collections/logs.coffee
@@ -5,6 +5,5 @@ define [
   class Logs extends Backbone.Collection
     model: Log
     sync: (method, model, options) ->
-      options.timeout = 10000
       options.dataType = 'jsonp'
       Backbone.sync(method, model, options)
--- apps/web/hubot-viewer/scripts/collections/logs.js
+++ apps/web/hubot-viewer/scripts/collections/logs.js
@@ -14,7 +14,6 @@
       Logs.prototype.model = Log;
 
       Logs.prototype.sync = function(method, model, options) {
-        options.timeout = 10000;
         options.dataType = 'jsonp';
         return Backbone.sync(method, model, options);
       };
